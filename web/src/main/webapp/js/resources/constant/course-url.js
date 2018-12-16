3d0788d37784d50b285c38299b5d7dc0df22a478
admin-kylan <daiyonglian1990@qq.com>
2018-12-17 00:16:04 +0800

    新增业绩分配模块

---
+++ web/src/main/webapp/js/resources/constant/course-url.js
@@ -0,0 +1,10 @@
+/**
+ * Created by Kylan on 2018/12/16.
+ */
+const COURSE_URL = {
+    marketShop: 'shop/getMarketShopList', //获取门店
+    checkAgreement: 'frAgreement/checkCardAgreement', //检查协议编号
+    addAgreement: 'frAgreement/addCardAgreement', //检查协议编号
+    marketUserList: 'personnelInfo/getMarketUserList', //获取销售人员
+    fetchSdaduim: 'shop/ShopSdaduimList', //选择场馆
+}
