3dcf0c3c2f038d824bd82b27a70a29c5aa1d6638
admin-kylan <daiyonglian1990@qq.com>
2018-12-21 00:01:25 +0800

    提交草稿版1

--- web/src/main/webapp/js/resources/constant/course-url.js
+++ web/src/main/webapp/js/resources/constant/course-url.js
@@ -7,5 +7,8 @@ const COURSE_URL = {
     addAgreement: 'frAgreement/addCardAgreement', //检查协议编号
     marketUserList: 'personnelInfo/getMarketUserList', //获取销售人员
     fetchSdaduim: 'shop/ShopSdaduimList', //选择场馆
-    fetchTechnician: 'frCustomerCourse/getMarketUserList', //
-}
+    fetchTechnician: 'frCustomerCourse/getMarketUserList', // 获取技师，助教
+    fetchUserInfo: 'frClient/getClient', //获取用户信息，根据用户Id
+    getOrderNo: 'frCardOrderInfo/getOrderNo', //获取订单编号
+    verifyPersonInfo: 'personnelInfo/getVerification', //用户授权验证
+};
